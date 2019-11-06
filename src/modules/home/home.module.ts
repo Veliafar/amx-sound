54d33686cd817502d5dac746f55033a84fc4d95e
Veliafar <denis.seo@nxt.ru>
2019-11-06 19:40:10 +0300

    add new logo

--- src/modules/home/home.module.ts
+++ src/modules/home/home.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomeHeaderComponent } from './home-header/home-header.component';
-import { HomeFooterComponent } from './home-footer/home-footer.component';
+import { HomeHeaderComponent } from './home/home-header/home-header.component';
+import { HomeFooterComponent } from './home/home-footer/home-footer.component';
 import { HomeComponent } from './home/home.component';
-import { HomeMainComponent } from './home-main/home-main.component';
+import { HomeMainComponent } from './home/home-main/home-main.component';
 
 
 @NgModule({
